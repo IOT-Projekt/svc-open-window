a3905b9dec9e37d24ed93b149d9deb1d402aead3
Patrik <128815730+patriklts@users.noreply.github.com>
2025-01-25 19:45:20 +0100

    added comments and did some formatting

--- app/main.py
+++ app/main.py
@@ -8,13 +8,16 @@ import os
 logging.basicConfig(level=logging.INFO)
 
 OPEN_WINDOW_STR = "Öffne die Fenster, um die Luftfeuchtigkeit zu vebessern."
-CLOSE_WINDOW_STR = "Bitte lasse die Fenster geschlossen, um die Luftfeuchtigkeit zu erhalten."
+CLOSE_WINDOW_STR = (
+    "Bitte lasse die Fenster geschlossen, um die Luftfeuchtigkeit zu erhalten."
+)
 
-# Kafka topics
+# Kafka topics from environment variables
 INDOOR_TOPIC = os.getenv("KAFKA_INDOOR_TOPIC", "humidity")
 OUTDOOR_TOPIC = os.getenv("KAFKA_OUTDOOR_TOPIC", "open_weather")
 
-def should_open_windows(indoor_humidity, outdoor_humidity):
+
+def should_open_windows(indoor_humidity, outdoor_humidity) -> str:
     """
     This function determines if you should open the windows based on the indoor and outdoor humidity.
     If the indoor humidity is below 40%, it recommends opening the windows. Between 40% and 60%, it always recommends closing the windows.
@@ -26,58 +29,73 @@ def should_open_windows(indoor_humidity, outdoor_humidity):
         return CLOSE_WINDOW_STR
     elif indoor_humidity > 60 and outdoor_humidity < indoor_humidity:
         return OPEN_WINDOW_STR
-    
+
     return CLOSE_WINDOW_STR
 
 
-def consume_indoor_humidity_messages(indoor_consumer, shared_data, lock):
+def consume_indoor_humidity_messages(indoor_consumer, shared_data, lock) -> None:
     for indoor_msg in indoor_consumer:
         # Get humidity and timestamp from message
         indoor_humidity = json.loads(indoor_msg.value.get("message"))["humidity"]
         logging.info(f"Received indoor humidity: {indoor_humidity}")
-        
+
         # update shared data with indoor humidity and check if we should open the windows
+        # use lock to prevent both threads from updating/reading shared data at the same time
         with lock:
             shared_data["indoor_humidity"] = indoor_humidity
             # if there is outdoor humidity data, check if we should open the windows
             if shared_data["outdoor_humidity"] is not None:
-                logging.info(should_open_windows(shared_data["indoor_humidity"], shared_data["outdoor_humidity"]))
-                shared_data["outdoor_humidity"] = None      # reset outdoor humidity data for next set of data
-    
+                # log the result if the window should be opened or closed
+                logging.info(
+                    should_open_windows(
+                        shared_data["indoor_humidity"], shared_data["outdoor_humidity"]
+                    )
+                )
+                shared_data["outdoor_humidity"] = (None)  # reset outdoor humidity data for next set of data
+                
 
 def consume_outdoor_humidity_messages(outdoor_consumer, shared_data, lock):
     for outdoor_msg in outdoor_consumer:
         # Get humidity and timestamp from message
         outdoor_humidity = json.loads(outdoor_msg.value.get("message"))["humidity"]
         logging.info(f"Received outdoor humidity: {outdoor_humidity}")
-        
+
         # update shared data with outdoor humidity and check if we should open the windows
+        # use lock to prevent both threads from updating/reading shared data at the same time
         with lock:
             shared_data["outdoor_humidity"] = outdoor_humidity
             # if there is indoor humidity data, check if we should open the windows
             if shared_data["indoor_humidity"] is not None:
-                logging.info(should_open_windows(shared_data["indoor_humidity"], shared_data["outdoor_humidity"]))
-                shared_data["indoor_humidity"] = None      # reset indoor humidity data for next set of data    
-    
+                # log the result if the window should be opened or closed
+                logging.info(
+                    should_open_windows(
+                        shared_data["indoor_humidity"], shared_data["outdoor_humidity"]
+                    )
+                )
+                shared_data["indoor_humidity"] = (None) # reset indoor humidity data for next set of data
+
+
 def main():
-    # Set up Kafka
+    # Set up the kafka consumer
     kafka_config = KafkaConfig()
     indoor_consumer = setup_kafka_consumer(kafka_config, [INDOOR_TOPIC])
     outdoor_consumer = setup_kafka_consumer(kafka_config, [OUTDOOR_TOPIC])
 
-    # Shared data between threads
+    # Create a dict of shared data between threads and a lock for thread safety
     shared_data = {
         "indoor_humidity": None,
         "outdoor_humidity": None,
     }
     lock = threading.Lock()
 
-    # Create the threads
+    # Create the threads with the functions to consume the messages
     indoor_thread = threading.Thread(
-        target=consume_indoor_humidity_messages, args=(indoor_consumer, shared_data, lock)
+        target=consume_indoor_humidity_messages,
+        args=(indoor_consumer, shared_data, lock),
     )
     outdoor_thread = threading.Thread(
-        target=consume_outdoor_humidity_messages, args=(outdoor_consumer, shared_data, lock)
+        target=consume_outdoor_humidity_messages,
+        args=(outdoor_consumer, shared_data, lock),
     )
 
     # Start the threads and wait for them to finish
@@ -86,6 +104,8 @@ def main():
     indoor_thread.join()
     outdoor_thread.join()
 
+
 if __name__ == "__main__":
+    # Run the main function in a loop
     while True:
         main()
